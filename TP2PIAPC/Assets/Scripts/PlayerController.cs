03c9e3cf27d0f3d6b66f457293a9b91feffe5023
julagui <julioaguilar-ap@outlook.com>
2021-09-20 22:22:12 -0300

    Seguimiento del tutorial
    Tutorial de Brackeys - NavMesh Unity Basics

---
+++ TP2PIAPC/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public UnityEngine.AI.NavMeshAgent agent;   
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
